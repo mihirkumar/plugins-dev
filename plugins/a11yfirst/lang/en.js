f98850ff744df87ddad0564392bb743979737681
Jon Gunderson <jongund@illinois.edu>
2017-07-10 15:27:47 -0500

    initial implementation of check link feature

--- plugins/a11yfirst/lang/en.js
+++ plugins/a11yfirst/lang/en.js
@@ -3,7 +3,7 @@ Copyright (c) 2003-2017, CKSource - Nicholas Hoyt, Jon Gunderson and Mihir Kumar
 For licensing, see LICENSE.md or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang( 'a11yfirst', 'en', {
-  msgEmptyLinkDisplayText: 'Using the a URL as link text is not accessible to people with disabilities:\nPress "Ok" to go back and add meaningful link text.\nPress "Cancel" to use the URL.',
-  msgPoorLinkDisplayText: 'The "%s" link text is not very descriptive of where the link will go to:\nPress "Ok" to go back and edit the description.\nPress "Cancel" to use the less accessible link text.',
+  msgEmptyLinkDisplayText: 'Using the URL as link text is not accessible to people with disabilities!\n\nPress "Ok" to go back and add meaningful link text.\n\nPress "Cancel" to use the URL.',
+  msgPoorLinkDisplayText: 'The "%s" link text is not very descriptive of where the link will goes!\n\nPress "Ok" to go back and edit the description.\n\nPress "Cancel" to use the less accessible link text.',
   poorLinkDisplayText: ['click', 'click here', 'more'],
 } );
